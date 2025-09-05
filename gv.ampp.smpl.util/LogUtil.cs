e23f61ca9ecf72e936d836059be5fc221b4890cc
saurabhimedia <45936098+saurabhimedia@users.noreply.github.com>
2025-09-05 19:04:11 +0530

    Ross 2nd batch

---
+++ gv.ampp.smpl.util/LogUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace gv.ampp.smpl.util
+{
+    public class LogUtil
+    {
+        public static String getCurrentDtTime()
+        {
+            return DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff");
+        }
+        public static void LogWithDtTime(String statement)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff':'") + statement);
+        }
+        
+    }
+}
